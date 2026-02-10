bd19c24364b215ba80c5dc2fea8a121995810fb1
mendisala28-bit <mendisala28@gmail.com>
2026-02-10 12:57:24 -0500

    Add files via upload

---
+++ Corte1/Teoria/Practica3/practica3.py
@@ -0,0 +1,9 @@
+while True:
+    a = input ("Por favor ingrese un valor")
+    aint = int(a)
+    mod = aint%2
+
+    if (mod == 0):
+        print ("a es par")
+    else:
+        print ("a es impar")
