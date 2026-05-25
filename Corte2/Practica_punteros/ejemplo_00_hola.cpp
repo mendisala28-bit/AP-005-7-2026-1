7c0619ec3dc6d381a8130a2a0589782d106c83cf
mendisala28-bit <mendisala28@gmail.com>
2026-05-25 06:45:32 -0500

    Delete Corte2 directory

--- Corte2/Practica_punteros/ejemplo_00_hola.cpp
+++
@@ -1,8 +0,0 @@
-// ejemplo_00_hola.cpp
-// Estructura minima de un programa en C++
-#include <iostream>
-
-int main() {
-    std::cout << "Hola desde C++." << std::endl;
-    return 0; // El programa finalizo correctamente.
-}
